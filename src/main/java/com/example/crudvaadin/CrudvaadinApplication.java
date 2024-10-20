3084e4503a9752bba4378360e67519f751b79ec1
Marcelo Gonçalves <joaogedavim787@gmail.comgit config --global user.name Marcelo>
2024-10-20 04:56:00 -0300

    first commit

---
+++ src/main/java/com/example/crudvaadin/CrudvaadinApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudvaadin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudvaadinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudvaadinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudvaadin/CrudvaadinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudvaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudvaadinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
